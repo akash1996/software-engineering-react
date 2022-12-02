eef2ab387f999acf07fa423b33b98c872ba78850
Akash Subramaniam Sethumurugan <s.akash1996@gmail.com>
2022-12-02 13:20:08 -0500

    A3 changes

---
+++ src/services/tuits-service.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+// const BASE_URL = "https://cs5500-01-sp22.herokuapp.com";
+
+const TUITS_API = 'https://softwareengineeringnode.herokuapp.com/api/tuits';
+const USERS_API = 'https://softwareengineeringnode.herokuapp.com/api/users';
+
+// const api = axios.create({
+//     withCredentials: true
+// });
+
+export const findAllTuits = () =>
+    axios.get(TUITS_API)
+        .then(response => response.data);
+
+export const findTuitById = (tid) =>
+    axios.get(`${TUITS_API}/${tid}`)
+        .then(response => response.data);
+
+export const findTuitByUser = (uid) =>
+    axios.get(`${USERS_API}/${uid}/tuits`)
+        .then(response => response.data);
+
+export const createTuit = (uid, tuit) =>
+    axios.post(`${USERS_API}/${uid}/tuits`, tuit)
+        .then(response => response.data);
+
+export const updateTuit = (tid, tuit) =>
+    axios.post(`${TUITS_API}/${tid}`, tuit)
+        .then(response => response.data);
+
+export const deleteTuit = (tid) =>
+    axios.delete(`${TUITS_API}/${tid}`)
+        .then(response => response.data);
+
+export const deleteTuitByContent = (content) =>
+        axios.get(`${TUITS_API}/${content}/delete`)
+            .then(response => response.data);
